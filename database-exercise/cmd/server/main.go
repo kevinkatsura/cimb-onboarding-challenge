50aac5160389f08509122953fa57f8d3d1c8c980
kevinkatsura <kevin.sitanggang55@gmail.com>
2026-03-16 13:32:31 +0700

    feat: create main function

---
+++ database-exercise/cmd/server/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"database-exercise/config"
+	"database-exercise/database"
+	"database-exercise/internal/handler"
+	"database-exercise/internal/repository"
+	"database-exercise/internal/service"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	cfg := config.LoadConfig()
+	db, err := database.NewGormConnection(cfg.DBSource)
+	if err != nil {
+		panic(err)
+	}
+	userRepo := repository.NewUserGormRepository(db)
+	userService := service.NewUserService(userRepo)
+	userHandler := handler.NewUserHandler(userService)
+
+	app := gin.Default()
+	app.POST("/users", userHandler.CreateUser)
+	app.GET("/users", userHandler.ListUsers)
+
+	app.Run(":8080")
+}
