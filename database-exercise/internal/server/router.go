c9188c11a0a60380df5e365c7c5ca86e0327c968
kevinkatsura <kevin.sitanggang55@gmail.com>
2026-03-17 12:29:45 +0700

    fix: fix path params for get by id and delete

--- database-exercise/internal/server/router.go
+++ database-exercise/internal/server/router.go
@@ -5,12 +5,16 @@ import (
 	"net/http"
 )
 
+func NewAPIPath(method, path string) string {
+	return method + " " + path
+}
+
 func NewRouter(userHandler *handler.UserHandler) http.Handler {
 	mux := http.NewServeMux()
-	mux.HandleFunc("GET /users", userHandler.ListUsers)
-	mux.HandleFunc("GET /users/:id", userHandler.GetUserByID)
-	mux.HandleFunc("POST /users", userHandler.CreateUser)
-	mux.HandleFunc("DELETE /users/:id", userHandler.DeleteUser)
+	mux.HandleFunc(NewAPIPath(http.MethodGet, "/users"), userHandler.ListUsers)
+	mux.HandleFunc(NewAPIPath(http.MethodGet, "/users/{id}"), userHandler.GetUserByID)
+	mux.HandleFunc(NewAPIPath(http.MethodPost, "/users"), userHandler.CreateUser)
+	mux.HandleFunc(NewAPIPath(http.MethodDelete, "/users/{id}"), userHandler.DeleteUser)
 
 	return Chain(
 		mux,
