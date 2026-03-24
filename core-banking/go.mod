860369a3d4e55d1397f94b790faeaa2eddc81a29
kevinkatsura <kevin.sitanggang55@gmail.com>
2026-03-25 00:14:39 +0700

    feat: add db config

--- core-banking/go.mod
+++ core-banking/go.mod
@@ -2,4 +2,7 @@ module core-banking
 
 go 1.25.6
 
-require github.com/jmoiron/sqlx v1.4.0 // indirect
+require (
+	github.com/jmoiron/sqlx v1.4.0 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+)
--- core-banking/internal/config/config.go
+++ core-banking/internal/config/config.go
@@ -1,1 +1,40 @@
 package config
+
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type DBConfig struct {
+	Host     string
+	Port     string
+	User     string
+	Password string
+	Name     string
+	SSLMode  string
+}
+
+func LoadConfig() *DBConfig {
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("No .env file found, using system env")
+	}
+
+	return &DBConfig{
+		Host:     getEnv("DB_HOST", "localhost"),
+		Port:     getEnv("DB_PORT", "5432"),
+		User:     getEnv("DB_USER", "postgres"),
+		Password: getEnv("DB_PASSWORD", "postgres"),
+		Name:     getEnv("DB_NAME", "banking"),
+		SSLMode:  getEnv("DB_SSLMODE", "disable"),
+	}
+}
+
+func getEnv(key, fallback string) string {
+	if val, ok := os.LookupEnv(key); ok {
+		return val
+	}
+	return fallback
+}
