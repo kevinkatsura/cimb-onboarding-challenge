66672563a109742b36a22b9b487dd0bf43293ce9
kevinkatsura <kevin.sitanggang55@gmail.com>
2026-03-24 16:21:43 +0700

    feat: create new index on accounts(deleted_at) to support partial indexing

--- core-banking/migrations/0002_indexes.down.sql
+++ core-banking/migrations/0002_indexes.down.sql
@@ -3,4 +3,5 @@ DROP UNIQUE INDEX IF EXISTS idx_unique_ledger;
 DROP INDEX IF EXISTS idx_ledger_account;
 DROP INDEX IF EXISTS idx_journal_transaction;
 DROP INDEX IF EXISTS idx_transactions_reference;
+DROP INDEX IF EXISTS idx_accounts_deleted_at;
 DROP INDEX IF EXISTS idx_accounts_customer;
--- core-banking/migrations/0002_indexes.up.sql
+++ core-banking/migrations/0002_indexes.up.sql
@@ -1,4 +1,5 @@
 CREATE INDEX IF NOT EXISTS idx_accounts_customer ON accounts(customer_id);                                    -- accounts
+CREATE INDEX IF NOT EXISTS idx_accounts_deleted_at ON accounts(deleted_at);                                   -- accounts
 CREATE INDEX IF NOT EXISTS idx_transactions_reference ON transactions(reference_id);                          -- transactions
 CREATE INDEX IF NOT EXISTS idx_journal_transaction ON journal_entries(transaction_id)                         -- journal_entries
 CREATE INDEX IF NOT EXISTS idx_ledger_account ON ledger_entries(account_id);                                  -- ledger_entries
