99ad70a269b51d78c7ddb66cc88b0da0630de8f9
kevinkatsura <kevin.sitanggang55@gmail.com>
2026-03-24 15:49:13 +0700

    feat: update routes for accounts

--- core-banking/cmd/api/main.go
+++ core-banking/cmd/api/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"core-banking/internal/database"
+	"core-banking/internal/modules/account"
 	"core-banking/internal/modules/transaction"
 	"log"
 	"net/http"
@@ -11,11 +12,25 @@ import (
 func main() {
 	db := database.NewDB(os.Getenv("DB_URL"))
 
-	repo := transaction.NewRepository(db)
-	service := transaction.NewService(repo)
-	handler := transaction.NewHandler(service)
+	// Transaction
+	txRepo := transaction.NewRepository(db)
+	txService := transaction.NewService(txRepo)
+	txHandler := transaction.NewHandler(txService)
 
-	http.HandleFunc("/transfer", handler.Transfer)
+	http.HandleFunc("POST /transfer", txHandler.Transfer)
+
+	// Account
+	accountRepo := account.NewRepository(db)
+	accountService := account.NewService(accountRepo)
+	accountHandler := account.NewHandler(accountService)
+
+	http.HandleFunc("POST /accounts", accountHandler.Create)
+	http.HandleFunc("GET /accounts", accountHandler.ListAll)
+	http.HandleFunc("GET /accounts/{id}", accountHandler.Get)
+	http.HandleFunc("PATCH /accounts/{id}", accountHandler.UpdateStatus)
+	http.HandleFunc("DELETE /accounts/{id}", accountHandler.Delete)
+
+	http.HandleFunc("GET /customers/{id}/accounts", accountHandler.List)
 
 	port := os.Getenv("PORT")
 	log.Println("server running on " + port)
