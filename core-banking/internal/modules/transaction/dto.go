7d20ed58ebd5f05dc360b703876ff9c9892098af
kevinkatsura <kevin.sitanggang55@gmail.com>
2026-03-24 20:05:15 +0700

    feat: create transaction history dto

---
+++ core-banking/internal/modules/transaction/dto.go
@@ -0,0 +1,27 @@
+package transaction
+
+import "time"
+
+type TransactionHistoryDTO struct {
+	TransactionID string `db:"transaction_id" json:"transaction_id"`
+	ReferenceID   string `db:"reference_id" json:"reference_id"`
+	ExternalRef   string `db:"external_reference" json:"external_reference"`
+
+	AccountID string `db:"account_id" json:"account_id"`
+
+	TransactionType string `db:"transaction_type" json:"transaction_type"`
+	Status          string `db:"status" json:"status"`
+
+	JournalType string `db:"journal_type" json:"journal_type"`
+
+	EntryType string `db:"entry_type" json:"entry_type"`
+
+	Amount       int64  `db:"amount" json:"amount"`
+	Currency     string `db:"currency" json:"currency"`
+	BalanceAfter int64  `db:"balance_after" json:"balance_after"`
+
+	Description string `db:"description" json:"description"`
+
+	CreatedAt   time.Time  `db:"created_at" json:"created_at"`
+	CompletedAt *time.Time `db:"completed_at" json:"completed_at"`
+}
