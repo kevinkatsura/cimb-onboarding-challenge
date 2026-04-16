616b471ddf5dcc3af8074a404977771fe4101ac0
kevinkatsura <kevin.sitanggang55@gmail.com>
2026-04-16 17:29:27 +0700

    [notification|feat] update gRPC system from custom to strong typed

--- penyedia-jasa-pembayaran/notification-service/cmd/migrate/main.go
+++ penyedia-jasa-pembayaran/notification-service/cmd/migrate/main.go
@@ -1,20 +1,28 @@
 package main
 
 import (
+	"fmt"
 	"notification-service/config"
 	"notification-service/pkg/database"
 	"notification-service/pkg/logging"
-	"fmt"
 	"os"
 )
 
 func main() {
 	logging.InitLogger()
-	if len(os.Args) < 2 { fmt.Println("Usage: ns-migrate [up|down]"); os.Exit(1) }
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: ns-migrate [up|down]")
+		os.Exit(1)
+	}
 	cfg := config.LoadConfig()
 	switch os.Args[1] {
-	case "up": database.EnsureDatabase(cfg); database.RunMigrateUp(cfg)
-	case "down": database.RunMigrateDown(cfg)
-	default: fmt.Printf("Unknown: %s\n", os.Args[1]); os.Exit(1)
+	case "up":
+		database.EnsureDatabase(cfg)
+		database.RunMigrateUp(cfg)
+	case "down":
+		database.RunMigrateDown(cfg)
+	default:
+		fmt.Printf("Unknown: %s\n", os.Args[1])
+		os.Exit(1)
 	}
 }
